af20b52f266de04e9857a4474c18c40edceb89ce
bhovesh2002 <110907973+bhovesh2002@users.noreply.github.com>
2023-08-30 15:21:24 +0530

    Create Product.java

---
+++ Day10/Product.java
@@ -0,0 +1,29 @@
+package Day10;
+
+public class Product {
+
+    private String productId;
+    private String productName;
+    private double price;
+
+//    public Product(){}
+
+    public Product(String productId, String productName, double price) {
+        this.productId = productId;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+}
